7c0cbadcf3853c6060de6af28efe924d452203cd
Felix Zheng <39350987+fezheng397@users.noreply.github.com>
2026-06-05 10:42:14 -0400

    Add env

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -8,10 +8,12 @@ import {
   withViewTransitions,
 } from "@angular/router";
 import { routes } from "./app.routes";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withFetch()),
     provideRouter(
       routes,
       withInMemoryScrolling({ scrollPositionRestoration: "top" }),
