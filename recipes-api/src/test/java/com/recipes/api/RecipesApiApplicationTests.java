5ff9fde220279546c30b87732f361a6c911787f5
Felix Zheng <39350987+fezheng397@users.noreply.github.com>
2026-06-05 13:23:39 -0400

    update deploy / db migration

---
+++ recipes-api/src/test/java/com/recipes/api/RecipesApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.recipes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApiApplicationTests {
+  @Test
+  void contextLoads() {}
+}
