7ea6fdf45977186356282d814e053a9a068173d0
Felix Zheng <39350987+fezheng397@users.noreply.github.com>
2026-06-14 09:03:12 -0400

    add media asset table

---
+++ recipes-api/src/main/java/com/recipes/api/media/entity/MediaAssetStatus.java
@@ -0,0 +1,8 @@
+package com.recipes.api.media.entity;
+
+public enum MediaAssetStatus {
+  PENDING_UPLOAD,
+  UPLOADED,
+  ATTACHED,
+  DELETED
+}
---
+++ recipes-api/src/test/java/com/recipes/api/media/entity/MediaAssetTests.java
@@ -0,0 +1,30 @@
+package com.recipes.api.media.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MediaAssetTests {
+  @Test
+  void advancesStatusThroughDomainMethods() {
+    MediaAsset asset =
+        new MediaAsset(
+            MediaType.IMAGE,
+            "recipes/status-test.jpg",
+            "https://media.jiawei.app/recipes/status-test.jpg",
+            null,
+            "image/jpeg",
+            4096);
+
+    assertEquals(MediaAssetStatus.PENDING_UPLOAD, asset.getStatus());
+
+    asset.markUploaded();
+    assertEquals(MediaAssetStatus.UPLOADED, asset.getStatus());
+
+    asset.markAttached();
+    assertEquals(MediaAssetStatus.ATTACHED, asset.getStatus());
+
+    asset.markDeleted();
+    assertEquals(MediaAssetStatus.DELETED, asset.getStatus());
+  }
+}
